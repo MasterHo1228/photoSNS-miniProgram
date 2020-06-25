19de566a58e0a49dde8af29052f1d2344b60ec5f
MasterHo1228 <masterho1228@gmail.com>
2020-06-25 16:42:36 +0800

    Learning part01-chapter05

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,21 @@
 Page({
-  
+  data:{
+    name: '路人X',
+
+    students:[
+      {ID:100,name:'Apple',age:18},
+      {ID:101,name:'Banana',age:16},
+      {ID:102,name:'Cat',age:17},
+      {ID:103,name:'Dog',age:22},
+      {ID:104,name:'Ewie',age:20}
+    ],
+
+    counter:0
+  },
+
+  hitCoder(){
+    this.setData({
+      counter:this.data.counter +1
+    });
+  }
 })
